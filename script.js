ca9a00b86c8a7d5f9a42e54b1082b4fe1b8441fb
Ternamdoo <jerryaliyese@gmail.com>
2025-06-04 15:45:56 +0100

    Color grid cells when the mouse hovers over them

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ for (let i = 1; i <= 16; i++) {
   }
   gridContainer.appendChild(gridColumn);
 }
+
+// Apply colorCell() to all the cells in the grid
+const cells = document.querySelectorAll('.grid-cell');
+cells.forEach((cell) => colorCell(cell));
+
+// Function to color a cell on mouse hover event
+function colorCell(cell) {
+  cell.addEventListener('mouseover', (event) => {
+    event.stopPropagation();
+    cell.style.backgroundColor = 'green';
+  })
+}
