9d884161419c02c9fcf746b06cae762acd98596e
Ternamdoo <jerryaliyese@gmail.com>
2025-06-05 02:31:13 +0100

    Add a button to allow users resize the grid
    Users can click on the button to enter a custom value for the number of
    cells they want on the grid.
    Only numbers within the range of 0 and 100 will be accepted.
    Floats will be floored to the nearest integer values.
    If a user enters a negative number, it's absolute value will be used
    instead.

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 // Select the grid container
 gridContainer = document.querySelector(".container");
 
-// Create grid cells
-for (let i = 1; i <= 16; i++) {
-  const gridColumn = document.createElement("div");
-  gridColumn.classList.add("grid-column", "grid-cell");
-
-  for (let j = 1; j <= 16; j++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row", "grid-cell");
-    gridColumn.appendChild(gridRow);
-
+const gridSize = 16;
+function createGrid(gridSize) {
+  for (let i = 1; i <= gridSize; i++) {
+    const gridColumn = document.createElement("div");
+    gridColumn.classList.add("grid-column", "grid-cell");
+  
+    for (let j = 1; j <= gridSize; j++) {
+      const gridRow = document.createElement("div");
+      gridRow.classList.add("grid-row", "grid-cell");
+      gridColumn.appendChild(gridRow);
+  
+    }
+    gridContainer.appendChild(gridColumn);
   }
-  gridContainer.appendChild(gridColumn);
 }
 
-// Apply colorCell() to all the cells in the grid
-const cells = document.querySelectorAll('.grid-cell');
-cells.forEach((cell) => colorCell(cell));
+// Create grid
+createGrid(gridSize);
 
+// Add  button to resize the grid
+const resizeGridBtn = document.createElement('button');
+resizeGridBtn.textContent = 'Grid Size';
+resizeGridBtn.classList.add('resize-grid-btn');
+gridContainer.parentNode.insertBefore(resizeGridBtn, gridContainer);
+
+resizeGridBtn.addEventListener('click', () => {
+  input = prompt('Enter a number between 0 and 100');
+  input = Math.abs(parseInt(input));
+  if (input !== NaN) {
+    if (input > 0 && input <= 100) {
+      // cells.forEach((cell) => cell.style.backgroundColor = 'white');
+      gridContainer.replaceChildren();
+      createGrid(input);
+      colorGrid();
+    } else {
+      alert('Please enter a number between 0 - 100!')
+    }
+  }
+});
+
+
+function colorGrid() {
+  // Apply colorCell() to all the cells in the grid
+  const cells = document.querySelectorAll('.grid-cell');
+cells.forEach((cell) => colorCell(cell));
+}
+colorGrid();
 
 function generateRandomNumber(number) {
   return Math.floor(Math.random() * (number + 1));
